ba26104736a7de67c4097b7510e9d09662e95215
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-25 20:03:41 -0800

    made tag model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,4 +4,5 @@ from .post import Post
 from .comment import Comment
 from .post_image import PostImage
 from .saveforlater import SaveForLater
+from .tag import Tag
 from .db import environment, SCHEMA
