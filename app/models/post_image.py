2d4643cccc5bc5512c2a349bcc8475c1fe98b7aa
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-25 18:58:13 -0800

    added post image model

---
+++ app/models/post_image.py
@@ -0,0 +1,23 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.sql import func
+
+class PostImage(db.Model):
+    __tablename__ = 'post_images'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('posts.id')), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=func.now())
+    updated_at = db.Column(db.DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'post_id': self.post_id,
+            'image_url': self.image_url,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
