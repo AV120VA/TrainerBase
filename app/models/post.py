2df7e9fd41e90cd159d6ba3be01e1fda0e4e464a
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-25 18:46:52 -0800

    post model migrated and upgraded

---
+++ app/models/post.py
@@ -0,0 +1,25 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.sql import func
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text(), nullable=True)
+    likes = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=func.now())
+    updated_at = db.Column(db.DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'likes': self.likes,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
