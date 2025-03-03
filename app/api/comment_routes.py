718533bc0cf69007a8188dd22caf595c185cff87
Aaron Vargas <vargasaaron62@gmail.com>
2025-03-02 21:27:56 -0800

    get comments for psot donew

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -4,3 +4,11 @@ from app.models import Comment
 from app.models.db import db
 
 comment_routes = Blueprint('comments', __name__)
+
+# Get all comments for a post
+@comment_routes.route('/post/<int:post_id>', methods=["GET"])
+def comments(post_id):
+    comments = Comment.query.filter(Comment.post_id == post_id)
+    if comments is None:
+        return jsonify({'message': 'No comments found'}), 404
+    return jsonify({"Comments": [comment.to_dict() for comment in comments]})
