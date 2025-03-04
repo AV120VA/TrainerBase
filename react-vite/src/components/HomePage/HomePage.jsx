1c4f8b6f9c045f1e613211a2a4e96e16fa757c42
Aaron Vargas <vargasaaron62@gmail.com>
2025-03-03 22:54:39 -0800

    made new post area to guide page aesthetic

---
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div className="home-page-container">
+      <div className="new-post-ta">
+        <textarea
+          className="new-post-input"
+          placeholder="What's on your mind?"
+        ></textarea>
+        <button className="new-post-button">Publish</button>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
