b9d3605f5c85a71c235164c880c1da01db481286
Aaron Vargas <vargasaaron62@gmail.com>
2025-03-03 23:53:07 -0800

    basic css, read red text

--- react-vite/src/components/Post/Post.jsx
+++ react-vite/src/components/Post/Post.jsx
@@ -1,9 +1,13 @@
-import React from "react";
 import "./Post.css";
 
 function Post({ post }) {
   return (
     <div className="post-container">
+      <div className="post-author-info">
+        <p className="author-name" style={{ color: "red" }}>
+          ADD INFO TO BACKEND ROUTE
+        </p>
+      </div>
       <h2>{post.title}</h2>
       <p>{post.content}</p>
       <p>Likes: {post.likes}</p>
