717c73e898cccbab4ae16333610a0f0798cefc9d
Aaron Vargas <vargasaaron62@gmail.com>
2025-03-04 20:47:02 -0800

    post structure done for now

--- react-vite/src/components/Post/Post.jsx
+++ react-vite/src/components/Post/Post.jsx
@@ -3,18 +3,29 @@ import "./Post.css";
 function Post({ post }) {
   return (
     <div className="post-container">
-      <div className="post-author-info">
-        <button disabled={true} className="username-initial-circle">
-          {post.User.username.slice(0, 1).toUpperCase()}
-        </button>
-        <div className="author-timestamp-box">
-          <h3 className="post-text">{post.User.username}</h3>
-          <p className="post-text">{post.created_at.slice(0, 16)}</p>
+      <div className="post-header-box">
+        <div className="post-author-info">
+          <button disabled={true} className="username-initial-circle">
+            {post.User.username.slice(0, 1).toUpperCase()}
+          </button>
+          <div className="author-timestamp-box">
+            <h3 className="post-text">{post.User.username}</h3>
+            <p className="post-text">{post.created_at.slice(0, 16)}</p>
+          </div>
         </div>
+        <button className="post-more">...</button>
       </div>
       <h3 className="post-text post-title">{post.title}</h3>
       <p className="post-text post-content">{post.content}</p>
       {post.PostImage && <img src={post.PostImage} alt="post" />}
+      <div className="post-reactions">
+        <img
+          src="/thumbs-up.png"
+          alt="thumbs up"
+          className="like-button-image"
+        />
+        <img src="/chat.png" alt="comment" className="comment-button-image" />
+      </div>
     </div>
   );
 }
