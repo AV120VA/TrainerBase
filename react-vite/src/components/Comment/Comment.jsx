ff131f749ac3032236c6741908ceb6e63f2bfedc
Aaron Vargas <vargasaaron62@gmail.com>
2025-03-10 00:32:12 -0700

    basic css 50%

--- react-vite/src/components/Comment/Comment.jsx
+++ react-vite/src/components/Comment/Comment.jsx
@@ -1,7 +1,19 @@
 import "./Comment.css";
 
 function Comment({ comment }) {
-  return <p style={{ color: "white" }}>{comment.content}</p>;
+  return (
+    <div className="comment-box">
+      <div className="comment-author-info">
+        <button disabled={true} className="comment-username-initial-circle">
+          {comment.User.username.slice(0, 1).toUpperCase()}
+        </button>
+        <div className="comment-author-timestamp-box">
+          <h3 className="comment-text">{comment.User.username}</h3>
+          <p className="comment-text">{comment.created_at.slice(0, 16)}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Comment;
