9218fa28a5574a9e1d9f2f68b44c3e565b29bbd2
Aaron Vargas <vargasaaron62@gmail.com>
2025-04-09 20:20:41 -0700

    create community form styled

--- react-vite/src/components/CreateCommunity/CreateCommunity.jsx
+++ react-vite/src/components/CreateCommunity/CreateCommunity.jsx
@@ -1,7 +1,22 @@
 import "./CreateCommunity.css";
 
 function CreateCommunity() {
-  return <h3 className="test">TEST</h3>;
+  return (
+    <form className="create-community-form">
+      <div className="create-community-inputs">
+        <input
+          className="create-community-name create-community-input"
+          type="text"
+          placeholder="Name Your Community..."
+        />
+        <textarea
+          className="create-community-description create-community-input"
+          placeholder="Description..."
+        ></textarea>
+      </div>
+      <button className="submit-community-button">Submit</button>
+    </form>
+  );
 }
 
 export default CreateCommunity;
