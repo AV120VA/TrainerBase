a2c4359fa9b271f41e311c45625594810c9fa561
Aaron Vargas <vargasaaron62@gmail.com>
2025-07-01 22:04:03 -0700

    os adjustment for dev

--- app/config.py
+++ app/config.py
@@ -9,6 +9,9 @@ class Config:
     # (only 'postgresql') but heroku's postgres add-on automatically sets the
     # url in the hidden config vars to start with postgres.
     # so the connection uri must be updated here (for production)
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+    db_url = os.environ.get('DATABASE_URL')
+    if db_url:
+        SQLALCHEMY_DATABASE_URI = db_url.replace('postgres://', 'postgresql://')
+    else:
+        raise RuntimeError('DATABASE_URL environment variable is not set. Please set it before running the application.')
     SQLALCHEMY_ECHO = True
